071f2a476ab2d2457efc3c77ea225d502e173d51
mark <youngce0918@gmail.com>
2019-12-13 00:39:47 +0800

    add data

--- Detector.py
+++ Detector.py
@@ -10,8 +10,12 @@ class Detector:
     #     self.img: np.ndarray = img
     @staticmethod
     def get_round_id(img: np.ndarray) -> str:
-        roi = ROI(216, 27, 259, 34)
+        roi = ROI(210, 25, 300, 40)
         img_cropped = img[roi.ys()[0]:roi.ys()[1], roi.xs()[0]:roi.xs()[1]]
+        cv2.rectangle(img, (roi.x,roi.y), (roi.x+roi.weight,roi.y+roi.high), (255, 0, 0), 3)
+        # cv2.imshow("full",img)
+        # cv2.imshow("cropped", img_cropped)
+
 
         return pytesseract.image_to_string(Image.fromarray(img_cropped), lang="eng")
 
@@ -28,3 +32,9 @@ class ROI:
 
     def xs(self) -> list:
         return [self.x, self.x + self.weight]
+
+# img=cv2.imread("./mydata/images/test2.png",cv2.IMREAD_GRAYSCALE)
+# roundId=Detector.get_round_id(img)
+# print(roundId)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
@@ -9,8 +10,22 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import credentials as cred
+import base64
+from Detector import Detector
+import os
+import cv2
+
+
+def take_screenshot_as_cv2img(driver):
+    import numpy as np
+    png = driver.get_screenshot_as_png()
+    nparr = np.frombuffer(png, np.uint8)
+    return cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+
+
 driver = webdriver.Firefox()
 # driver = webdriver.Firefox(executable_path='./geckodriver.exe')
+
 driver.get("https://www.gamesofa.com/landlord/?op=login")
 
 driver.find_element_by_id("username").send_keys(cred.username)
@@ -19,6 +34,8 @@ driver.find_element_by_id("password").send_keys(cred.password)
 driver.find_element_by_id("login").click()
 driver.find_element_by_class_name("landlord").click()
 
+
+
 while True:
     if len(driver.window_handles) > 1:
         driver.switch_to.window(driver.window_handles[1])
@@ -26,27 +43,39 @@ while True:
         # driver.switch_to_window()
     time.sleep(1)
 
-#
-# wait = WebDriverWait(driver, 100)
-# startBtn = wait.until(ec.h((By.ID, "start")))
-# ActionChains(driver).move_to_element(startBtn).perform()
-
-
-
-import base64
-
-import cv2
-
-cv2.imshow(base64.b64decode(driver.get_screenshot_as_base64()))
-cv2.imdecode()
 driver.find_element_by_xpath("//div[@id=\"start\"]/a").click()
-# while True:
-#     if len(driver.window_handles) > 2:
-#         driver.switch_to.window(driver.window_handles[2])
-#         break
-#         # driver.switch_to_window()
-#     print("sleep...")
-#     time.sleep(1)
-# wait = WebDriverWait(driver, 10)
-# cxn = wait.until(ec.visibility_of_element_located((By.XPATH, "//div[id=\"Cocos2dGameContainer\"]")))
-#
+
+while True:
+    if len(driver.window_handles) > 2:
+        driver.switch_to.window(driver.window_handles[2])
+        break
+        # driver.switch_to_window()
+    print("sleep...")
+    time.sleep(1)
+wait = WebDriverWait(driver, 10)
+cxn = wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@id=\"Cocos2dGameContainer\"]")))
+
+while True:
+    img = take_screenshot_as_cv2img(driver)
+    roundId = Detector.get_round_id(img)
+
+
+    print("round: " + roundId)
+    if roundId:
+        folder = "./mydata/rounds/%s" % roundId
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+        else:
+            millis = int(round(time.time() * 1000))
+            file = "%s/%s.png" % (roundId, millis)
+
+            cv2.imwrite("%s/%s.jpg" % (folder, millis), img)
+            print("saved")
+    else:
+        cv2.imshow("debug",img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    print("sleep...")
+    time.sleep(5)
+
+# print("roundId"roundId)
